4f5245ae61bf9282e1cec431bfa3780f2e53e3c3
Fabiano Fernandes <contato@fabianofernandes.adm.br>
2025-04-15 17:07:34 -0300

    wip: cadastro e edicao de cfop na aplicacao central

---
+++ app/Filament/Resources/CfopResource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CfopResource\Pages;
+use App\Filament\Resources\CfopResource\RelationManagers;
+use App\Models\Cfop;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CfopResource extends Resource
+{
+    protected static ?string $model = Cfop::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Configurações';
+
+    protected static ?string $navigationLabel = 'CFOPs';
+    
+    
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('codigo')
+                            ->required()
+                            ->label('Código')
+                            ->unique(ignoreRecord: true)
+                            ->maxLength(4),
+                        Forms\Components\TextInput::make('descricao')
+                            ->required()
+                            ->label('Descrição')
+                            ->maxLength(255),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->recordUrl(null)
+            ->columns([
+                Tables\Columns\TextColumn::make('codigo')
+                    ->label('Código')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('descricao')
+                    ->label('Descrição')
+                    ->sortable()
+                    ->limit(70)
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCfops::route('/'),
+            'create' => Pages\CreateCfop::route('/create'),
+            'edit' => Pages\EditCfop::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/CfopResource/Pages/EditCfop.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\CfopResource\Pages;
+
+use App\Filament\Resources\CfopResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCfop extends EditRecord
+{
+    protected static string $resource = CfopResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
